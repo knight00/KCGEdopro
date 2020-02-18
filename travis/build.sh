e454b39beab1b71eb2e0d8fe0ac4f586eed70f9c
kevinlul <6320810+kevinlul@users.noreply.github.com>
2020-02-17 20:59:25 -0500

    Try to speed up builds by not specifying job slots and updating homebrew

--- travis/build.sh
+++ travis/build.sh
@@ -12,9 +12,9 @@ if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
 fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     ./premake5 gmake2 --vcpkg-root=$VCPKG_ROOT --sound=sdl-mixer --pics=\"$PICS_URL\" --fields=\"$FIELDS_URL\" --discord=\"$DISCORD_APP_ID\"
-    make -Cbuild -j2 config=$BUILD_CONFIG ygoprodll
+    make -Cbuild -j config=$BUILD_CONFIG ygoprodll
 fi
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     ./premake5 gmake2 --vcpkg-root=$VCPKG_ROOT --sound=sdl-mixer --pics=\"$PICS_URL\" --fields=\"$FIELDS_URL\" --discord=\"$DISCORD_APP_ID\"
-    make -Cbuild -j2 config=$BUILD_CONFIG ygoprodll
+    make -Cbuild -j config=$BUILD_CONFIG ygoprodll
 fi
